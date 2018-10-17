84cc850730927aa22d7a6145ca512d3e296f3867
warroyo <warroyo7199008@gmail.com>
2018-10-17 10:33:02 -0600

    adding correct params

--- tasks/service/task.sh
+++ tasks/service/task.sh
@@ -5,4 +5,4 @@ set -o pipefail
 
 apk add --no-cache jq
 
-kubectl --insecure-skip-tls-verify --namespace $k8s_namespace get service $k8s_service -o json | jq -r '.status.loadBalancer.ingress | .[0].ip' > out/service_ip
+kubectl --insecure-skip-tls-verify --server $k8s_server  --token $k8s_token --namespace $k8s_namespace get service $k8s_service -o json | jq -r '.status.loadBalancer.ingress | .[0].ip' > out/service_ip
